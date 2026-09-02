44d07d36af7efd6352b76c15d234337673a8524a
stncttr908 <stncttr908@users.noreply.github.com>
2026-09-02 12:09:48 -0400

    fix(ota): restore user brightness level when OTA update is skipped or fails

--- M5NSWebConfig.cpp
+++ M5NSWebConfig.cpp
@@ -549,6 +549,7 @@ void otaRunUpdate() {
       M5.Lcd.setTextColor(RED);
       M5.Lcd.println("UPDATE FAILED");
       delay(3000);
+      setBrightness(screenOn ? lcdBrightness : 0);
       M5.Lcd.fillScreen(BLACK);
       draw_page();
       break;
@@ -558,6 +559,7 @@ void otaRunUpdate() {
       M5.Lcd.setTextColor(YELLOW);
       M5.Lcd.println("NO UPDATES");
       delay(3000);
+      setBrightness(screenOn ? lcdBrightness : 0);
       M5.Lcd.fillScreen(BLACK);
       draw_page();
       break;
@@ -674,6 +676,7 @@ void handleUpdate() {
   if (!cfg.is_task_bootstrapping) {
     M5.update();
     delay(2000);
+    setBrightness(screenOn ? lcdBrightness : 0);
     M5.Lcd.fillScreen(BLACK);
     draw_page();
     if(cfg.LED_strip_mode != 0) { 
